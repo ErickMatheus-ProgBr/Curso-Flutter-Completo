4409613145828fae53af516ef348c6988a7acab6
DEVerick <erickmatheuscruz@gmail.com>
2025-10-08 16:27:54 -0300

    ok

---
+++ aa/2.operacoes_matematicos.dart
@@ -0,0 +1,45 @@
+// void main(){
+//     print(1 + 3);
+//     print(1 + 2 - 1 * 5);
+// }
+
+// -------------------------------
+
+// void main(){
+//   print(1 + (2 - 1)* 5);
+//   print((1 + 2 - 1 ) * 5);
+//   print(1 + (2 - 1  * 5));
+//   // podemos calcular da maneira que a gente quiser,
+//   // sempre lembrando as regras, qual é o correto, e qual vem primeiro.
+// }
+
+// -------------------------------
+
+// void main(){;
+//     print(12.5 * 2);
+// }
+
+// -------------------------------
+
+
+// void main(){
+//   print("erick " * 34);
+//   // ou
+//   print("3" * 44);
+// }
+
+// -------------------------------
+
+// Não podemos fazer desta maneira porque vai dar errado; 
+
+// void main(){
+//   print("3" * "2");
+// }
+
+// -------------------------------
+
+// se utilizarmos o + na soma, vai dar certo porque vamos esta concatenando dois valores 
+
+void main(){
+  print("0" + "1234");
+}
